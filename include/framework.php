77167e8d0e9a391df8200de1b9dd5c7b55e1d5db
Maxim Folko <max.folko@gmail.com>
2017-06-22 08:10:29 +0000

    framework.php edited online with Bitbucket include file

--- include/framework.php
+++ include/framework.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: maxfo
- * Date: 6/18/2017
- * Time: 8:51 PM
- */
+
+function loadScripts(){
+    wp_enqueue_script('framework-script', get_template_directory_uri() . '/framework/dist/js/scripts.min.js', false, '', true);
+    wp_enqueue_style('framework-style', get_template_directory_uri() . '/framework/dist/css/main.min.css');
+}
+
+add_action('wp_enqueue_scripts', 'loadScripts');
